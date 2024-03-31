f9c767d2926f6fcfe175b2da0421df13d9526c24
stray-code <abc1007020@gmail.com>
2024-03-31 11:50:00 +0900

    manifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,27 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: 'サイトブロック',
   version: '1.0.0',
+  icons: {
+    16: 'assets/img/icon16.png',
+    48: 'assets/img/icon48.png',
+    128: 'assets/img/icon128.png'
+  },
+  action: {
+    default_icon: 'assets/img/icon16.png',
+    default_popup: 'src/popup/index.html',
+  },
+  content_scripts: [
+    {
+      js: ['src/content/main.ts'],
+      matches: ['http://*/*', 'https://*/*'],
+    }
+  ],
+  options_page: "src/option/index.html",
+  background: {
+    service_worker: "src/background.ts",
+    type: "module"
+  },
+  permissions: ["webNavigation", "storage"],
 });
 
 // https://vitejs.dev/config/
