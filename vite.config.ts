5da862a9d778be69aab71e5111f10cbb83430fbc
stray-code <abc1007020@gmail.com>
2024-03-31 14:01:00 +0900

    descriptionを追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { crx, defineManifest } from '@crxjs/vite-plugin'
 const manifest = defineManifest({
   manifest_version: 3,
   name: 'サイトブロック',
+  description: 'サイトを閲覧できないようにします',
   version: '1.0.0',
   icons: {
     16: 'assets/img/icon16.png',
